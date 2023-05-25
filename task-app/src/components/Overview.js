4d83595cd484e970a4bc6f6933b3f4ba5ad68d30
man7788 <jerman1841@gmail.com>
2023-05-26 06:22:28 +0800

    Change item font size

--- task-app/src/components/Overview.js
+++ task-app/src/components/Overview.js
@@ -4,10 +4,14 @@ const Overview = (props) => {
   const { tasks } = props;
 
   return (
-    <ul style={{ listStyleType: 'none' }}>
+    <ul
+      style={{
+        listStyleType: 'none',
+      }}
+    >
       {tasks.map((task) => {
         return (
-          <li key={task.id} id={task.id}>
+          <li key={task.id} id={task.id} style={{ fontSize: '18.26px' }}>
             {task.order + 1 + '. '}
             {task.text}
           </li>
