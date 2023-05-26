3966c5b48ff206d0dd083368c74c8fc065e54366
man7788 <jerman1841@gmail.com>
2023-05-27 01:56:05 +0800

    Add edit function

--- task-app/src/App.js
+++ task-app/src/App.js
@@ -13,6 +13,12 @@ class App extends Component {
         order: '',
       },
       tasks: [],
+      edits: [],
+      editTask: {
+        text: '',
+        id: uniqid(),
+        order: '',
+      },
     };
   }
 
@@ -28,18 +34,32 @@ class App extends Component {
 
   onSubmitTask = (e) => {
     e.preventDefault();
+    const newEdit = {
+      display: (
+        <button
+          key={this.state.task.id}
+          id={this.state.task.id}
+          onClick={this.onEdit}
+        >
+          Edit
+        </button>
+      ),
+      id: this.state.task.id,
+    };
     this.setState({
       tasks: this.state.tasks.concat(this.state.task),
       task: {
         text: '',
         id: uniqid(),
       },
+      edits: this.state.edits.concat(newEdit),
     });
   };
 
   onDelete = (e) => {
     const targetId = e.target.id;
 
+    const newEdits = this.state.edits.filter((task) => task.id !== targetId);
     let newTasks = this.state.tasks.filter((task) => task.id !== targetId);
 
     newTasks = newTasks.map((task, index) => {
@@ -49,11 +69,91 @@ class App extends Component {
 
     this.setState({
       tasks: newTasks,
+      edits: newEdits,
+    });
+  };
+
+  onEdit = (e) => {
+    const targetId = e.target.id;
+
+    let newEdits = this.state.edits.map((edit, index) => {
+      if (edit.id === targetId) {
+        edit.index = index;
+        edit.display = undefined;
+        edit.field = (
+          <form onSubmit={this.onSubmitEdit}>
+            <input onChange={this.handleEdit} type="text" id={edit.id} />
+            <button type="submit">Submit</button>
+          </form>
+        );
+        return edit;
+      } else {
+        return edit;
+      }
+    });
+
+    this.setState({
+      edits: newEdits,
+    });
+  };
+
+  handleEdit = (e) => {
+    const id = e.target.id;
+    let order;
+
+    this.state.tasks.forEach((task) => {
+      if (task.id === id) {
+        order = task.order;
+      }
+    });
+
+    this.setState({
+      editTask: {
+        text: e.target.value,
+        id: this.state.editTask.id,
+        order: order,
+      },
+    });
+  };
+
+  onSubmitEdit = (e) => {
+    e.preventDefault();
+    const newEdit = {
+      display: (
+        <button
+          key={this.state.editTask.id}
+          id={this.state.editTask.id}
+          onClick={this.onEdit}
+        >
+          Edit
+        </button>
+      ),
+      id: this.state.editTask.id,
+      field: undefined,
+    };
+
+    const order = this.state.editTask.order;
+    const newTasks = this.state.tasks.map((task) => {
+      return task;
+    });
+    const newEdits = this.state.edits.map((task) => {
+      return task;
+    });
+    newTasks.splice(order, 1, this.state.editTask);
+    newEdits.splice(order, 1, newEdit);
+
+    this.setState({
+      tasks: newTasks,
+      editTask: {
+        text: '',
+        id: uniqid(),
+      },
+      edits: newEdits,
     });
   };
 
   render() {
-    const { task, tasks } = this.state;
+    const { task, tasks, edits } = this.state;
 
     return (
       <div>
@@ -73,16 +173,39 @@ class App extends Component {
             style={{
               listStyleType: 'none',
               display: 'flex',
-              flexDirection: 'column',
             }}
           >
-            {tasks.map((task) => {
-              return (
-                <button key={task.id} id={task.id} onClick={this.onDelete}>
-                  Delete
-                </button>
-              );
-            })}
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+              }}
+            >
+              {tasks.map((task) => {
+                return (
+                  <div key={task.id}>
+                    <button key={task.id} id={task.id} onClick={this.onDelete}>
+                      Delete
+                    </button>
+                  </div>
+                );
+              })}
+            </div>
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+              }}
+            >
+              {edits.map((edit) => {
+                return (
+                  <div key={edit.id}>
+                    {edit.field}
+                    {edit.display}
+                  </div>
+                );
+              })}
+            </div>
           </ul>
         </div>
       </div>
